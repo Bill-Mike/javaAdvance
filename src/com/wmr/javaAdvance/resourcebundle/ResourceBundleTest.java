9886affe5c82a211d465bb103c12c65666f5fe84
mrwang <wangmaorui@idrilling.com>
2021-03-19 09:56:40 +0800

    java 基础类库：CharSequence, AutoCloseable, StringBuffer, Runtime测试

--- src/com/wmr/javaAdvance/resourcebundle/ResourceBundleTest.java
+++ src/com/wmr/javaAdvance/resourcebundle/ResourceBundleTest.java
@@ -1,11 +1,15 @@
 package com.wmr.javaAdvance.resourcebundle;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceBundleTest {
     public static void main(String[] args) {
-        ResourceBundle res = ResourceBundle.getBundle("com.wmr.javaAdvance.resourcebundle.test");
+        Locale locale = new Locale("zh_CN");
+        ResourceBundle res = ResourceBundle.getBundle("com.wmr.javaAdvance.resourcebundle.test",locale);
         String value = res.getString("test");
+        System.out.println(value);
+        value = res.getString("info");
         System.out.println(value);
     }
 }
