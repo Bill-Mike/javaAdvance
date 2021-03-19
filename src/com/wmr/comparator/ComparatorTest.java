d289e05038dd2bf982b477fd609282750bf8b192
wangmaorui@idrilling.com <wangmaorui@idrilling.com>
2021-03-19 17:08:16 +0800

    添加比较器测试

--- src/com/wmr/comparator/ComparatorTest.java
+++ src/com/wmr/comparator/ComparatorTest.java
@@ -1,4 +1,51 @@
 package com.wmr.comparator;
 
+import java.util.Arrays;
+
 public class ComparatorTest {
+    public static void main(String[] args) {
+        int[] data = new int[]{3,6,4,77,44,22,88,99,100};
+        Arrays.sort(data);
+        System.out.println(Arrays.toString(data));
+    }
+
+}
+
+class Person implements Comparable{
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
