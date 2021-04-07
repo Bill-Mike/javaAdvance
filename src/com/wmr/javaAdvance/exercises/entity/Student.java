d544058e8f767adc39d9e0c11518be36b7cbe509
wangmaorui@idrilling.com <wangmaorui@idrilling.com>
2021-04-07 15:30:29 +0800

    添加字符串反序输出操作

---
+++ src/com/wmr/javaAdvance/exercises/entity/Student.java
@@ -0,0 +1,36 @@
+package com.wmr.javaAdvance.exercises.entity;
+
+public class Student implements Comparable<Student>{
+    private String name ;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.score - o.getScore();
+    }
+
+}
---
+++ src/com/wmr/javaAdvance/exercises/service/IStudenService.java
@@ -0,0 +1,7 @@
+package com.wmr.javaAdvance.exercises.service;
+
+import com.wmr.javaAdvance.exercises.entity.Student;
+
+public interface IStudenService {
+    public Student[] getData(); //获取排序后的数字
+}
