a5fb1af235318d9c6134f9c630a6aa80795548c7
wangmaorui@idrilling.com <wangmaorui@idrilling.com>
2021-04-12 15:42:41 +0800

    修改反射工厂，添加泛型操作，是工厂模式适用于所有接口

--- src/com/wmr/javaAdvance/reflect/ReflectAndFactory.java
+++ src/com/wmr/javaAdvance/reflect/ReflectAndFactory.java
@@ -3,8 +3,10 @@ package com.wmr.javaAdvance.reflect;
 public class ReflectAndFactory {
     public static void main(String[] args) {
         try {
-            IMessage msg = Factory.getInstance("com.wmr.javaAdvance.reflect.NetMessage");
+            IMessage msg = Factory.getInstance(IMessage.class,"com.wmr.javaAdvance.reflect.NetMessage");
             msg.send();
+            IService service = Factory.getInstance(IService.class,"com.wmr.javaAdvance.reflect.HouseService");
+            service.service();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,10 +23,21 @@ class NetMessage implements IMessage{
     }
 }
 
+interface IService{
+    public void service();
+}
+
+class HouseService implements IService{
+    @Override
+    public void service() {
+        System.out.println("提供房子的打扫服务！");
+    }
+}
+
 class Factory{
     private Factory(){}
 
-    public static IMessage getInstance(String className) throws Exception{
-        return (IMessage) Class.forName(className).newInstance();
+    public static <T> T getInstance(Class<T> T, String className) throws Exception{
+        return (T) Class.forName(className).newInstance();
     }
 }
